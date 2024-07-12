9eaf889f728c2853b6c80ca2398ff9a512f5d101
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-07-11 21:48:40 -0300

    Processando formulario

---
+++ process_form.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('Location: /contact.php');
+    exit;
+}
+
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+    $email = $_POST['email'];
+    
+    if ($email === 'gabrieleduardomonsani@hotmail.com') {
+        header('Location: /contact.php?success');
+        exit;
+    } else {
+        header('Location: /contact.php?error');
+        exit;
+    }
+} else {
+    header('Location: /contact.php?error');
+    exit;
+}
+?>
