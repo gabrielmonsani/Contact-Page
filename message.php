92fab8ea87dc86c252942c406f85722ee72fabc8
Gabriel Eduardo Monsani <gabrieleduardomonsani@hotmail.com>
2024-07-18 19:39:07 -0300

    Criação de Objeto

---
+++ message.php
@@ -0,0 +1,37 @@
+<?php
+
+class Mensagem {
+    private $conteudo;
+    private $remetente;
+    private $destinatario;
+
+    public function __construct($conteudo, $remetente, $destinatario) {
+        $this->conteudo = $conteudo;
+        $this->remetente = $remetente;
+        $this->destinatario = $destinatario;
+    }
+
+    public function getConteudo() {
+        return $this->conteudo;
+    }
+
+    public function setConteudo($conteudo) {
+        $this->conteudo = $conteudo;
+    }
+
+    public function getRemetente() {
+        return $this->remetente;
+    }
+
+    public function setRemetente($remetente) {
+        $this->remetente = $remetente;
+    }
+
+    public function getDestinatario() {
+        return $this->destinatario;
+    }
+
+    public function setDestinatario($destinatario) {
+        $this->destinatario = $destinatario;
+    }
+}
--- process_form.php
+++ process_form.php
@@ -1,14 +1,20 @@
 <?php
+require_once 'message.php';
+
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     header('Location: index.php');
     exit;
 }
 
 if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['message'])) {
+    $nome = $_POST['name'];
     $email = $_POST['email'];
-    
-    if ($email === 'gabrieleduardomonsani@hotmail.com') {
-        header('Location: http://localhost/bootcamp/contactpage/sucess.php');
+    $mensagemTexto = $_POST['message'];
+
+    $mensagem = new Mensagem($mensagemTexto, $email, 'gabrieleduardomonsani@hotmail.com');
+
+    if ($mensagem->getRemetente() === 'gabrieleduardomonsani@hotmail.com') {
+        header('Location: sucess.php');
         exit;
     } else {
         header('Location: index.php?error');
